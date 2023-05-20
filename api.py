64bf736b1884b231c68ca9e16c3d76a9d441dcf8
eriktheninjadog <erik.tamm@eriktamm.com>
2023-05-20 21:59:55 +0800

    done it

--- api.py
+++ api.py
@@ -64,6 +64,7 @@ def is_valid_part(part):
 
 def create_api_question_on_cws(question,cwsid,segmentfunction,type,restriction):
     stored_cws = database.get_cws_by_id(cwsid)
+    print("len " + str(len(stored_cws.orgtext.strip())))
     if not restriction(stored_cws.orgtext.strip()):
         return
     text_to_split = stored_cws.orgtext
