5e38000645e68b7d789ac539a15f6874826c3476
eriktheninjadog <erik.tamm@eriktamm.com>
2023-05-22 11:08:38 +0800

    ddd

--- api.py
+++ api.py
@@ -133,6 +133,8 @@ def lookup_position(cwsid,position):
     ret.append(acwsret)
     for h in hits:
         log.log(' here is the response object:' +  str(h))
+        storedcws = get_cws_text(h.responsecwsid)
+        ret.append(storedcws)
         #responsecwsid = h[]
     return ret
 
