a5e05916232a859d33ef5332db7f82c084848f30
eriktheninjadog <erik.tamm@eriktamm.com>
2023-06-27 13:50:48 +0800

    done d

--- articlecollector.py
+++ articlecollector.py
@@ -1,6 +1,9 @@
 import api
 import articlecrawler
 from newspaper import Article
+import requests
+from bs4 import BeautifulSoup
+
 
 
 def getanarticle():
@@ -12,5 +15,17 @@ def getanarticle():
         if len(article.text) > 300:
             print(article.title)
             api.create_verify_challenge(article.text)
-            
-getanarticle()
+
+
+#def getworldnews():
+#getanarticle()
+response = requests.get('https://www.reuters.com/news/archive/worldNews')
+# parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+# find all the links on the main page
+links = soup.find_all('a')
+# print the href attribute of each link
+for link in links:
+    print(link.get('href'))
+
+
