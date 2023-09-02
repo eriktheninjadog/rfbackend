6fdec451f4ea460d36242751142a32fb3d199d11
eriktheninjadog <erik.tamm@eriktamm.com>
2023-09-02 15:54:17 +0800

    done d

--- webapi.py
+++ webapi.py
@@ -344,7 +344,7 @@ def get_look_up_history():
     lookups = database.lookup_history(cwsid)
     return jsonify({'result':lookups})
 
-@app.route('/get_classification',methods=['POST'])
+@app.route('/   ',methods=['POST'])
 def get_classification():
     cwsid = request.json['cwsid']
     cws = api.get_cws_text( cwsid )    
