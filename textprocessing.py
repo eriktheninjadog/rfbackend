1026be8fd65bb84b8d513318c48f97232fc2fe26
eriktheninjadog <erik.tamm@eriktamm.com>
2023-09-02 16:05:17 +0800

    done d

--- textprocessing.py
+++ textprocessing.py
@@ -34,8 +34,8 @@ def split_text(text):
 def get_word_class(text):
     text_parts = pseg.cut(text)
     ret_parts = {}
-    for p,pos in text_parts:
-        ret_parts[p] = pos
+    for p in text_parts:
+        ret_parts[p.word] = p.flag
     return ret_parts
 
 def split_text_parts(text):
