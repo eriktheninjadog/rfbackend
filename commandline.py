a180b6e83915a7b98889bd69d648926fc245cf86
eriktheninjadog <erik.tamm@eriktamm.com>
2023-09-07 09:42:23 +0800

    done d

--- commandline.py
+++ commandline.py
@@ -40,6 +40,8 @@ def import_file(bookname,filename):
     counter = 0
     for c in chunks:
         cws = api.process_chinese(bookname + str(counter),"import",c,constants.CWS_TYPE_IMPORT_TEXT,-1)
+        print(str(cws.id))
+        exit(-1)
         batchprocessing.apply_ai_to_cws(cws.id,"Rewrite this using chinese using short sentences, words that a child old would understand and put a _ before all personal names:"
         )
         counter += 1
