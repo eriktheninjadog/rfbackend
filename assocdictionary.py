3761b8e795a89c74ecad7f9d8cc20193b889f313
eriktheninjadog <erik.tamm@eriktamm.com>
2023-09-14 08:56:52 +0800

    done d

--- assocdictionary.py
+++ assocdictionary.py
@@ -33,18 +33,6 @@ for c in freq:
 
 count = 0
 for c in freq:
-    filename = '/var/www/html/api/rfbackend/' + c + '.chardesc' 
-    if os.path.exists(filename) == True:
-        fp = open(filename,"r",encoding='UTF-8')
-        text = fp.read()
-        api.process_chinese(''+c,"ai",text,constants.CWS_TYPE_DETAILED_CHAR_INFO,-1)
-        count+= 1
-        if (count > 20000):
-            exit(-1)
-"""
-    if database.find_word(c) == None:
-        dd = dd + ' ' + c
-        if (len (dd) > 500):
-            api.process_chinese('imported char','process',dd, constants.CWS_TYPE_IMPORT_TEXT,-1)
-            dd = ''
-"""
+    ask = "break down the traditional character '"+c+"' into its traditional parts, give an example of it use and make a one line story to remember it"
+    ret = aisocketapi.ask_ai(ask)
+    api.process_chinese(''+c,"ai",ret,constants.CWS_TYPE_DETAILED_CHAR_INFO,-1)
