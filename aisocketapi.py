7082100c5784e5b952b89c8dc809743942b0a1f0
eriktheninjadog <erik.tamm@eriktamm.com>
2023-10-16 22:52:57 +0800

    done d

--- aisocketapi.py
+++ aisocketapi.py
@@ -60,6 +60,10 @@ def ask_ai(question):
                     response += chunk
                     if not firstgotten:
                         firstgotten = True
+                        athing = chunk.decode()
+                        parts = athing.split("\n\r\n\r")
+                        print(str(parts))
+                        
                     else:
                         #everything start with the length in hex followed by line and data:
                         #is the data encoded, is it binary?
