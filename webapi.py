983a52acab55459fffce248ed79d7b9cc6bf9ce9
eriktheninjadog <erik.tamm@eriktamm.com>
2024-03-18 08:59:06 +0800

    done d

--- webapi.py
+++ webapi.py
@@ -632,7 +632,7 @@ def poeexamples():
 
 def create_poe_example_question(level,number_of_sentences):
     text = "Write " + str(number_of_sentences) + " example sentences in English at a " + level + " level of difficulty, along with their Cantonese translation, in a dictionary in JSON format without any other text."    
-    text = "Create 10 sentences at A1 level including the following words: bear, ninja, dog, guess,waterfall,homecoming. Return these together with cantonese tranlsation in json format like this: [{\"english\":ENGLISH_SENTENCE,\"chinese\":CANTONESE_TRANSLATION}].Only respond with the json structure."
+    text = "Create "+ str(number_of_sentences) +" sentences at A1 level including some the following words: " + wordlists.get_sample_A1_wordlist(30)+ ". Return these together with cantonese tranlsation in json format like this: [{\"english\":ENGLISH_SENTENCE,\"chinese\":CANTONESE_TRANSLATION}].Only respond with the json structure."
     return text
 
 def is_list(obj):
