91f0f2d03d428b8c09e4ab1890eda5f6f751748a
eriktheninjadog <erik.tamm@eriktamm.com>
2024-09-10 08:22:32 +0800

    done d

--- audioimport.py
+++ audioimport.py
@@ -41,7 +41,7 @@ import subprocess
 def add_mp3_to_database(file_path):
     # Database connection setup
     print("Here comes the add_mp3_to_database")
-    print("type of filename is " + type(file_path))
+    print("type of filename is " + str(type(file_path)))
     db,cursor  = get_db_connection()
     # Get the file name from the path
     file_name = os.path.basename(file_path)    
