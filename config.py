42c0c0bc4c2481abf78f2f1de0ddb358d577e1c4
eriktheninjadog <erik.tamm@eriktamm.com>
2024-11-25 11:58:12 +0800

    done d

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 import json
 def get_config_value(name):
-    f = open('/home/www/html/scene/wwwconfig.json','r')
+    f = open('/var/www/html/scene/wwwconfig.json','r')
     dict = json.loads(f.read())
     f.close()
     return dict[name]
