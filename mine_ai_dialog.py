bc2bd159bf2aa0f6ca48e9f26edca7a16a4a96bf
eriktheninjadog <erik.tamm@eriktamm.com>
2025-02-12 12:06:33 +0800

    done d

--- mine_ai_dialog.py
+++ mine_ai_dialog.py
@@ -29,6 +29,7 @@ for b in blop:
     result += b
 
 print(str(blop))
+result = api.open_router_claude_3_5_sonnet("You are a language teaching expert, helping teachers to make their tutoring more efficient","From this lesson transcript, write notes what the student needs to practice on:" + result)
 result = api.open_router_nova_micro_v1("Extract and organise the chinese from this text mass: " + result)
 remotechineseclient.access_remote_client("make_c1_examples", {"pattern": "use vocabulary, syntax, expressions and grammatics from this text:" +result})
  
