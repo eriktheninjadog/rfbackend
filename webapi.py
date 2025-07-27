d538393cc6b000345a54d271e3ac972f8bf458b1
eriktheninjadog <erik.tamm@eriktamm.com>
2025-07-27 10:44:46 +0800

    eon e

--- webapi.py
+++ webapi.py
@@ -2179,7 +2179,7 @@ def get_time():
 
     return jsonify({
         "activity_name": activity_name,
-        "accumulated_time": int(accumulated_time,
+        "accumulated_time": int(accumulated_time),
         "total_accumulated_time":int(total_accumulated_time)
     }), 200
 
